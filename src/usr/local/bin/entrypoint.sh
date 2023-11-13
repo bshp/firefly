92c4ba172cf9e953cf3dc1e09ef2ae1f38dfee62
Jason A Everling <jason.everling@gmail.com>
2023-11-12 22:11:37 -0600

    name changes

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -16,18 +16,18 @@ VADC_IP_HEADER=${VADC_IP_HEADER:-X-Forwarded-For};
 /usr/local/bin/updateCerts.sh ${CA_URL}
     
 ## Initialization ##
-VADC_IP_REG=$(echo "${VADC_IP_ADDRESS}" | sed -e 's/\s/\|/g')
-TC_PROXIES=$(echo "${VADC_IP_REG}" | sed -e 's/\./\\./g')
-TC_OPTS=$(echo 'export JAVA_OPTS="'$JAVA_OPTS $APP_PARAMS'"')
-export VADC_IP_REG=${TC_PROXIES}
+FIREFLY_IP_REGEX=$(echo "${VADC_IP_ADDRESS}" | sed -e 's/\s/\|/g')
+FIREFLY_PROXIES=$(echo "${FIREFLY_IP_REGEX}" | sed -e 's/\./\\./g')
+FIREFLY_OPTS=$(echo 'export JAVA_OPTS="'$JAVA_OPTS $APP_PARAMS'"')
+export VADC_IP_REG=${FIREFLY_PROXIES}
 echo "export VADC_IP_ADDRESS=${VADC_IP_ADDRESS}" >> /etc/apache2/envvars
 echo "export VADC_IP_HEADER=${VADC_IP_HEADER}" >> /etc/apache2/envvars
-echo $TC_OPTS > /opt/tomcat/bin/setenv.sh && chmod -R 0755 /opt/tomcat/bin/setenv.sh
+echo $FIREFLY_OPTS > /opt/tomcat/bin/setenv.sh && chmod -R 0755 /opt/tomcat/bin/setenv.sh
     
 ## Print Configs ##
 echo "Remote IP: Apache2 module configured with address: ${VADC_IP_ADDRESS} and header: ${VADC_IP_HEADER}"
-echo "Remote IP: Tomcat RemoteIpValve configured with remoteIpHeader: ${VADC_IP_HEADER} and internalProxies: ${VADC_IP_REG}"
-echo "Tomcat: Configured JAVA_OPTS with ${TC_OPTS}"
+echo "Remote IP: Tomcat RemoteIpValve configured with remoteIpHeader: ${VADC_IP_HEADER} and internalProxies: ${FIREFLY_PROXIES}"
+echo "Tomcat: Configured JAVA_OPTS with ${FIREFLY_OPTS}"
 echo "Initialization complete, attempting to start container as: [Apache: www-data, Tomcat: tomcat]"
     
 # Start Services:
